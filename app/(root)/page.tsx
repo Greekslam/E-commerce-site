7a74140c8fd32827217e895bbda2603bf2089dbf
Godson Imiebo <imiebo.godson@gmail.com>
2024-02-06 09:43:06 +0100

    fix: auth flow fix

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col gap-4 items-start p-5">
       <div>
-        <UserButton afterSignOutUrl="/" />
+        <UserButton afterSignOutUrl="/sign-in" />
       </div>
       <div className="h-screen w-full flex flex-col gap-4 justify-center items-center">
         <div className="text-base">KEEP CALM, WE DEY COOOOK🧑‍🍳🥘!</div>
